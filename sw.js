78c4227ccb68ebfab363c9ed5a8bb92080c6570b
wendleypf <wendley.franca@ccc.ufcg.edu.br>
2018-01-14 14:18:19 -0300

    pwa not exists

--- sw.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw-builder.js')
-        .then(function () {
-            console.log('service worker registered.');
-        })
-        .catch(function () {
-            console.warn('service worker failed.');
-        });
-}
